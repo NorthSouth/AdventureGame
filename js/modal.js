7a09cd17054adba2ffd8cc09cc242b6b04c2fe8c
NorthSouth <nschnepf@hotmail.com>
2018-02-08 06:44:29 -0800

    decreased spacing

--- js/modal.js
+++ js/modal.js
@@ -6,39 +6,39 @@ var span = document.getElementsByClassName("close")[0];
 
 
 function showModal() {
-    modal.style.display = "block";
+  modal.style.display = "block";
 }
 
 
 function closeModal() {
-    modal.style.display = "none";
+  modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
 function restartGame() {
-    modal.style.display = "none";
-    clearBoard();
-    document.getElementById("startButton").style.visibility = "visible";
+  modal.style.display = "none";
+  clearBoard();
+  document.getElementById("startButton").style.visibility = "visible";
 
-    obstacles = [];
-    for (i = 0; i < ((maxXIndex + 1) * (maxYIndex + 1)); i++) {
-        obstacles.push(0);
-    }
+  obstacles = [];
+  for (i = 0; i < ((maxXIndex + 1) * (maxYIndex + 1)); i++) {
+    obstacles.push(0);
+  }
 
-    startGame();
+  startGame();
 }
 
 function endGame() {
-    closeModal();
-    audioClip = document.getElementById("myAudio");
-    audioClip.pause();
-    document.getElementById("canvasArea").style.visibility = "hidden";
-    document.getElementById("actionArea").style.visibility = "hidden";
-    document.getElementById("appArea").style.visibility = "hidden";
+  closeModal();
+  audioClip = document.getElementById("myAudio");
+  audioClip.pause();
+  document.getElementById("canvasArea").style.visibility = "hidden";
+  document.getElementById("actionArea").style.visibility = "hidden";
+  document.getElementById("appArea").style.visibility = "hidden";
 }
