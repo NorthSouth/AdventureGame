20278eceaa7225f0fc5d4111b5935f191888b29d
NorthSouth <nschnepf@hotmail.com>
2018-02-07 22:49:26 -0800

    init repo

---
+++ js/modal.js
@@ -0,0 +1,44 @@
+// Get the modal
+var modal = document.getElementById('modal');
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+function showModal() {
+    modal.style.display = "block";
+}
+
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function restartGame() {
+    modal.style.display = "none";
+    clearBoard();
+    document.getElementById("startButton").style.visibility = "visible";
+
+    obstacles = [];
+    for (i = 0; i < ((maxXIndex + 1) * (maxYIndex + 1)); i++) {
+        obstacles.push(0);
+    }
+
+    startGame();
+}
+
+function endGame() {
+    closeModal();
+    audioClip = document.getElementById("myAudio");
+    audioClip.pause();
+    document.getElementById("canvasArea").style.visibility = "hidden";
+    document.getElementById("actionArea").style.visibility = "hidden";
+    document.getElementById("appArea").style.visibility = "hidden";
+}
